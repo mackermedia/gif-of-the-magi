7409d62f44b407f708d7971add0e39e9ed4abebc
Mike Ackerman <mikeackerman@gmail.com>
2014-04-01 21:00:40 -0600

    Temporary viget ip whitelist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+  before_filter :validate_viget_ip
+
+  private
+
+  def validate_viget_ip
+    last_octet = request.ip.split('.').last
+    hq_start   = request.ip.start_with?("70.182.186")
+
+    case request.ip
+    when "50.52.128.102"
+      return true
+    when "173.164.61.149"
+      return true
+    when hq_start && last_octet >= 97 && last_octet <= 126
+      return true
+    when "70.182.186.100"
+      return true
+    when "127.0.0.1"
+      return true
+    end
+
+    false
+  end
 end
