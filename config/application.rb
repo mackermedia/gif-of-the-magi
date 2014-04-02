298207cf9359c82133f601633a9da29a8105eba5
Mike Ackerman <mikeackerman@gmail.com>
2014-04-02 10:50:55 -0600

    Precompile scripts.js

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(:default, Rails.env)
 
 module GifOfTheMagi
   class Application < Rails::Application
-    
+
     config.time_zone = 'Eastern Time (US & Canada)'
 
     config.autoload_paths += %W(#{config.root}/lib)
@@ -19,6 +19,10 @@ module GifOfTheMagi
       g.assets false
       g.helper false
     end
+
+    config.assets.precompile += [
+      'scripts.js'
+    ]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
